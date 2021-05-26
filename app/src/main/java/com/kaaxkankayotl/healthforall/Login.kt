76bedc921b521abeb4ee38a96488e387e589af39
grisreyesrios <grisell.reyes.rios@gmail.com>
2021-05-25 22:28:54 -0500

    Code until second release, version code 7, version 1.5.2 and validation of email and password

--- app/src/main/java/com/kaaxkankayotl/healthforall/Login.kt
+++ app/src/main/java/com/kaaxkankayotl/healthforall/Login.kt
@@ -10,6 +10,7 @@ import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.core.util.PatternsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -17,6 +18,7 @@ import com.google.firebase.ktx.Firebase
 
 
 import com.kaaxkankayotl.healthforall.databinding.ActivityLoginBinding
+import java.util.regex.Pattern
 import android.widget.Toast.makeText as widgetToastMakeText
 
 
@@ -34,33 +36,6 @@ class Login : AppCompatActivity() {
         supportActionBar?.hide()
         auth = Firebase.auth
 
-        binding.buttonsignup.setOnClickListener {
-            if (binding.userET.text.toString().isEmpty() || binding.passET.text.toString()
-                            .isEmpty()){
-                val toast = Toast.makeText(applicationContext, "Email Address or Password is not provided",
-                    Toast.LENGTH_SHORT)
-                toast.show()
-            } else {
-                auth.createUserWithEmailAndPassword(
-                        binding.userET.text.toString(),
-                        binding.passET.text.toString())
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                val toast = Toast.makeText(applicationContext, "Sign Up successfully. You are registered",
-                                    Toast.LENGTH_SHORT)
-                                toast.show()
-                                val user = auth.currentUser
-                                updateUI(user)
-                            } else {
-                                val toast = Toast.makeText(applicationContext, "You are already register or there was a problem",
-                                    Toast.LENGTH_SHORT)
-                                toast.show()
-                                updateUI(null)
-                            }
-                        }
-            }
-        }
-
         binding.alreadyRegistered.setOnClickListener {
             val intent = Intent(this, SignIn::class.java)
             startActivity(intent)
@@ -68,8 +43,95 @@ class Login : AppCompatActivity() {
         }
 
 
+        binding.buttonsignup.setOnClickListener {
+            validate()
+           // auth.createUserWithEmailAndPassword(
+                //binding.userET.text.toString(),
+                //binding.passET.text.toString())
+                //.addOnCompleteListener(this) { task ->
+                    //if (task.isSuccessful) {
+                        //val toast = Toast.makeText(applicationContext, "Sign Up successfully. You are registered",
+                            //Toast.LENGTH_SHORT)
+                        //toast.show()
+                        //val user = auth.currentUser
+                        //updateUI(user)
+                    //} else {
+                        //val toast = Toast.makeText(applicationContext, "You are already register or there was a problem",
+                            //Toast.LENGTH_SHORT)
+                        //toast.show()
+                        //updateUI(null)
+                         //   }
+                        //}
+            }
+        }
+    private fun validate(){
+        val result = arrayOf(validateEmail(), validatePassword())
 
+        if (false in result){
+            return
+        }else{
+            auth.createUserWithEmailAndPassword(
+                binding.userET.text.toString(),
+                binding.passET.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        val toast = Toast.makeText(
+                            applicationContext, "You are already register or there was a problem",
+                            Toast.LENGTH_SHORT
+                        )
+                        toast.show()
+                        updateUI(null)
+                    }
+                }
+
+        }
+
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
     }
+
+    private fun validateEmail():Boolean {
+        val email = binding.userET.editableText.toString()
+
+        return if(email.isEmpty()){
+            binding.userET.error = "Field cannot be empty"
+            false
+        } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.userET.error = "Field cannot be empty"
+            false
+        }else{
+            binding.userET.error = null
+            true
+        }
+    }
+
+    private fun validatePassword(): Boolean {
+        val password = binding.passET.editableText.toString()
+        val passwordRegex = Pattern.compile(
+            "^" +
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{6,}" +               //at least 4 characters
+                    "$")
+
+        return if(password.isEmpty()){
+            binding.passET.error = "Field cannot be empty"
+            false
+        } else if(!passwordRegex.matcher(password).matches()){
+            binding.passET.error = "Password is too weak!"
+            false
+        }else{
+            binding.passET.error = null
+            true
+        }
+    }
+
 
     private fun updateUI(currentUser: FirebaseUser?) {
         val intent = Intent(this, MainActivity::class.java)
@@ -77,4 +139,7 @@ class Login : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
-}
+    }
+
+
+
